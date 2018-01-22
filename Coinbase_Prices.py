ce10a2443f3ab0b64eb9127a5d702f8ebecad3b7
Jonathan Blake <jonawayneblake@gmail.com>
2018-01-21 22:16:32 -0500

    updated API key

--- Coinbase_Prices.py
+++ Coinbase_Prices.py
@@ -9,7 +9,7 @@ from coinbase.wallet.client import *
 import sys
 
 # Need to enter new api key and secret 
-my_client = Client(api_key="api_key", api_secret="secret", api_version="https://api.coinbase.com/"+datetime.datetime.today().strftime('%Y-%m-%d'))
+my_client = Client(api_key="SKuPyYAGVLuq6QGA", api_secret="kFiOHUMNTeJZD7dT0KqQ8HEOIrffsFcs", api_version="https://api.coinbase.com/"+datetime.datetime.today().strftime('%Y-%m-%d'))
 
 # Get Datetime info
 year = datetime.datetime.now().strftime("%Y")
@@ -23,7 +23,7 @@ def get_btcbuy_price(self, currency_pair):
     buy_price = my_client.get_buy_price(currency_pair = 'BTC-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Buy Price: $%s : Timestamp: %s" % (buy_price.amount, timestamp)
-    return(year,month,day,hour,minute,second,str('Buy Price'),buy_price.amount,buy_price.base,buy_price.currency)
+    return(year,month,day,hour,minute,second,str('Buy Price'), buy_price.amount,buy_price.base, buy_price.currency)
 
 def get_btcspot_price(self,currency_pair):
     spot_price = my_client.get_spot_price(currency_pair='BTC-USD')
@@ -38,37 +38,37 @@ def get_btcsell_price(self, currency_pair):
     return(year,month,day,hour,minute,second,str('Sell Price'),sell_price.amount,sell_price.base, sell_price.currency)
 
 def get_ethbuy_price(self, currency_pair):
-    buy_price = my_client.get_buy_price(currency_pair = "ETH-USD")
+    buy_price = my_client.get_buy_price(currency_pair = 'ETH-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Buy Price: $%s : Timestamp: %s" % (buy_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Buy Price'), buy_price.amount,buy_price.base, buy_price.currency)
 
 def get_ethspot_price(self, currency_pair):
-    spot_price = my_client.get_spot_price(currency_pair= "ETH-USD")
+    spot_price = my_client.get_spot_price(currency_pair= 'ETH-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Spot Price: $%s : Timestamp: %s" %(spot_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Spot Price'), spot_price.amount,spot_price.base, spot_price.currency)
 
 def get_ethsell_price(self, currency_pair):
-    sell_price = my_client.get_sell_price(currency_pair ="ETH-USD")
+    sell_price = my_client.get_sell_price(currency_pair = 'ETH-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Sell Price: $%s : Timestamp: %s" %(sell_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Sell Price'),sell_price.amount,sell_price.base, sell_price.currency)
 
 def get_ltcbuy_price(self, currency_pair):
-    buy_price = my_client.get_buy_price(currency_pair = "LTC-USD")
+    buy_price = my_client.get_buy_price(currency_pair = 'LTC-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Buy Price: $%s : Timestamp: %s" % (buy_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Buy Price'), buy_price.amount,buy_price.base, buy_price.currency)
 
 def get_ltcspot_price(self, currency_pair):
-    spot_price = my_client.get_spot_price(currency_pair= "LTC-USD")
+    spot_price = my_client.get_spot_price(currency_pair= 'LTC-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Spot Price: $%s : Timestamp: %s" %(spot_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Spot Price'), spot_price.amount,spot_price.base, spot_price.currency)
 
 def get_ltcsell_price(self, currency_pair):
-    sell_price = my_client.get_sell_price(currency_pair ="LTC-USD")
+    sell_price = my_client.get_sell_price(currency_pair='LTC-USD')
     timestamp = datetime.datetime.now()
     #price_stamp = "Sell Price: $%s : Timestamp: %s" %(sell_price.amount, timestamp)
     return(year,month,day,hour,minute,second,str('Sell Price'),sell_price.amount,sell_price.base, sell_price.currency)
